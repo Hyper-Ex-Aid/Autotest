5b249f180700e032429d53533429a8984ca4c280
Hyper-Ex-Aid <smokykk@163.com>
2024-05-04 16:56:46 +0800

    修改

--- test_vector.py
+++ test_vector.py
@@ -3,6 +3,12 @@ from vector import Vector
 
 
 class TestVector(unittest.TestCase):
+    def setUp(self):
+        print("开始测试")
+
+    def tearDown(self):
+        print("结束测试")
+
     def test_init(self):
         v = Vector(1, 2)
         self.assertEqual(v.x, 1)
